98caa845dfa9937cd4483e0d09a4f584a6bc8d45
jennlindley <jennifersu2059@gmail.com>
2020-03-23 18:05:37 -0500

    portafolio casi terminado

--- main.js
+++ main.js
@@ -28,6 +28,31 @@ window.addEventListener('load', () => {
         grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
     });
 
+    // Agregando listener a las imagenes
+
+    const overlay = document.getElementById('overlay');
+    document.querySelectorAll('.grid .item img').forEach((elemento) => {
+        elemento.addEventListener('click', () => {
+            const ruta = elemento.getAttribute('src');
+            const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+            
+            overlay.classList.add('activo');
+            document.querySelector('#overlay img').src = ruta;
+            document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+        });
+    });
+
+    // Evenlistener del boton cerrar
+
+    document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+        overlay.classList.remove('activo');
+
+    });
+
+    // Eventlistener del overlay
+
+    overlay.addEventListener('click', (evento) => {
+        evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+    })
+
 });
-
-
